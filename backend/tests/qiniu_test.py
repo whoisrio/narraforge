259e94d89adc5f8ca37e79b455681531399f705c
rio.dong <rio.dong@hotmail.com>
2026-05-21 17:17:56 +0800

    modify qiniu_test demo

--- backend/tests/qiniu_test.py
+++ backend/tests/qiniu_test.py
@@ -1,3 +1,28 @@
-from qiniu import Auth
+from qiniu import Auth, put_file_v2,etag
+import qiniu.config
+from dotenv import load_dotenv
+import os
 
-q = Auth('','')
+load_dotenv()
+ak = os.getenv("OSS_AK")
+sk = os.getenv("OSS_SK")
+buck_name = os.getenv("BUCKET_NAME")
+file = 'backend/uploads/voices/tts_579cd234-6458-499a-b9e3-9065d5850123.mp3'
+
+key = 'tts_579cd234-6458-499a-b9e3-9065d5850123.mp3'
+
+# 认证
+q = Auth(ak,sk)
+
+# 获取token
+token = q.upload_token(buck_name,key,3600)
+
+# 执行上传
+ret,info = put_file_v2(token,key,file,version='v2')
+
+print(info)
+
+assert ret['key'] == key
+assert ret['hash'] == etag(file)
+
+
