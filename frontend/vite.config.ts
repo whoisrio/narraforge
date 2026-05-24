0a93dee0cb86a358add5feb20614c85c10be5eb7
rio.dong <rio.dong@hotmail.com>
2026-05-24 18:04:03 +0800

    bugfix: dockerfile

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8002',
+        target: 'http://backend:8000',
         changeOrigin: true,
       },
     },
